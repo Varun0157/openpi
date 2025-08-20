6f8a9401104894a77f3522d21420609d385d14ad
Varun0157 <varun.edachali@gmail.com>
2025-08-20 23:14:25 +0530

    Revert "reduce len of dataset"
    This reverts commit 3c9681a0c86c9e7c04f842410e3041b2670010ec.

--- src/openpi/training/droid_rlds_dataset.py
+++ src/openpi/training/droid_rlds_dataset.py
@@ -159,4 +159,4 @@ class DroidRldsDataset:
     def __len__(self):
         # This is the approximate number of samples in DROID after filtering.
         # Easier to hardcode than to iterate through the dataset and compute it.
-        return 20_000
+        return 20_000_000
