35e393c0c25bd242dfa5f113ae1cfc23ab7b9456
Varun0157 <varun.edachali@gmail.com>
2025-08-21 13:01:29 +0530

    changge active process

--- src/openpi/training/checkpoints.py
+++ src/openpi/training/checkpoints.py
@@ -50,8 +50,7 @@ def initialize_checkpoint_dir(
             create=False,
             async_options=ocp.AsyncOptions(timeout_secs=7200),
             multiprocessing_options=ocp.options.MultiprocessingOptions(
-                primary_host=0,  # Default primary
-                active_processes={0},  # Limit to process 0; expand as needed
+                active_processes={1},  # Limit to process 0; expand as needed
             ),
         ),
     )
