3f785fc36139219ed61d4d4d2d73044ac7fa492b
Varun0157 <varun.edachali@gmail.com>
2025-09-12 01:30:55 +0530

    assume 50k samples

--- src/openpi/training/droid_rlds_dataset.py
+++ src/openpi/training/droid_rlds_dataset.py
@@ -153,4 +153,4 @@ class DroidRldsDataset:
     def __len__(self):
         # This is the approximate number of samples in DROID after filtering.
         # Easier to hardcode than to iterate through the dataset and compute it.
-        return 37_600
+        return 50_000
